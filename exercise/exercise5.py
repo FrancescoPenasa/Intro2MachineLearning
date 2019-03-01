4231cc97a5f1be68779cc4ecf94d2c366c96b99d
Francesco Penasa <francesco.penasa-1@studenti.unitn.it>
2019-03-01 18:30:53 +0100

    week 2

--- exercise/exercise5.py
+++ exercise/exercise5.py
@@ -2,9 +2,44 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Feb 26 09:52:00 2019
-
 @author: francesco
+
+Given a 1-dimensional vector v with n rows and a 2-dimensional matrix M with n columns, 
+without using any built-in function, define functions to:
+ - compute the inner product of two vectors v1 and v2
+ - compute the product of x and M
+ 
+TIP: you can use Python lists
 """
 
 import numpy as np
-help(numpy)
+#help(np)
+#help(np.linalg)
+v = np.array([2,1,3])
+M = np.array([[1,2,3],[4,5,6],[7,8,9]])
+
+def inner_product(v1, v2):
+    """compute the inner product of two vectors v1 and v2"""
+    v1 = np.matrix(v1)
+    v2 = np.matrix(v2)
+    print ("Inner product: ", v1*v2)
+    
+def product(v, M):
+    """compute the product of x and M"""
+    inner_product(v, M)
+    
+product(v, M)
+
+
+####other way
+if v.shape[0] == v.size:
+    res = np.zeros((1,M.shape[0]))
+else:
+    res = np.zeros((v.shape[1],M.shape[0]))
+    
+    
+for column in range(v.shape[0]):
+    for j in range(M.shape[0]):
+        res[0][column] += v[j]*M[j][column]
+
+print(res)
--- exercise/exercise6.py
+++ exercise/exercise6.py
@@ -2,8 +2,36 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Feb 26 10:10:18 2019
-
 @author: francesco
+
+Given the iris dataset CSV file and a new unseen vector representing a flower, 
+define a function that classifies the new flower according to the class of its nearest neighbor.
+TIPS: 
+    * use the Euclidean distance as distance metric
+    * you can download and find more information about the iris dataset at 
+    https://archive.ics.uci.edu/ml/datasets/iris.
 """
 
 import pandas as pd
+import numpy as np
+
+
+new_flower = np.array([5.2,2.3,3.2,1.1])
+
+df = pd.read_csv("iris.data", names = ["param0","param1", "param2", "param3", "name"])
+
+def near_flower(df, new_flower):
+    lowest_distance = np.inf
+    lowest_flower_index = -1
+    for n in range(len(df)):
+        old_flower = df.iloc[n,0:4]    
+        res = np.linalg.norm(old_flower - new_flower)
+        if lowest_distance >= res:
+            lowest_distance = res
+            lowest_flower_index = n;
+    print("new flower: ", new_flower,)
+    print(lowest_flower_index)
+    print("is a: ", df.iloc[lowest_flower_index][4])
+    print(df.iloc[lowest_flower_index])
+
+near_flower(df, new_flower)
